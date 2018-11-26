6ac551f0a2ecaba9c9dc04cd0dc2622f61e01a93
EC2 Default User <ec2-user@ip-172-31-28-14.us-east-2.compute.internal>
2018-11-26 14:00:29 +0000

    public2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -92,5 +92,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.public_file_server.enabled = true
 end
