09d58814504c3b2104550e025645593e035da841
EC2 Default User <ec2-user@ip-172-31-28-14.us-east-2.compute.internal>
2018-11-26 13:36:36 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create(name: 'aaa', symbol: 'AAA', price:30000)
+PriceLog.create(price:300000, coin_id:1, high:300000, low:200000)
+PriceLog.create(price:400000, coin_id:1, high:300000, low:200000)
+PriceLog.create(price:500000, coin_id:1, high:300000, low:200000)
+PriceLog.create(price:600000, coin_id:1, high:300000, low:200000)
+PriceLog.create(price:700000, coin_id:1, high:300000, low:200000)
+
+Coin.create(name: 'bbb', symbol: 'BBB', price:30000)
